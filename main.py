05d97ead5bb2bb3aca7b81048a3a11e102172cf2
Magomedow Ruslan <main@mail.ru>
2025-03-23 16:07:05 +0300

    add /{hotel_id} to main.py

--- main.py
+++ main.py
@@ -19,16 +19,6 @@ def get_hotels(city: str | None = Query(default=None)):
     return [hotel for hotel in hotels_db if hotel["city"] == city]
 
 
-@app.delete("/hotels/{hotel_id}", summary="delete hotel")
-def delete_hotel(hotel_id: int):
-    """ delete hotel by key hotel_id """
-    for index, hotel in enumerate(hotels_db):
-        if hotel["id"] == hotel_id:
-            hotels_db.pop(index)
-            return {"status": 200, "message": "OK"}
-    return {"status": 404, "message": "Not Found"}
-
-
 @app.post("/hotels", summary="create hotel")
 def create_hotel(
         hotel_name: str | None = Body(default=None, embed=True),
@@ -45,9 +35,9 @@ def create_hotel(
     return {"status": 422, "message": "Bad Data"}
 
 
-@app.put("/hotels", summary="edit hotel")
+@app.put("/hotels/{hotel_id}", summary="edit hotel")
 def edit_hotel(
-        hotel_id: int = Query(),
+        hotel_id: int,
         hotel_name: str = Body(embed=True),
         city: str = Body(embed=True)
 ):
@@ -60,9 +50,9 @@ def edit_hotel(
     return {"status": 422, "message": "Bad Data"}
 
 
-@app.patch("/hotel", summary="partial edit hotel")
+@app.patch("/hotel/{hotel_id}", summary="partial edit hotel")
 def partial_edit_hotel(
-        hotel_id: int = Query(),
+        hotel_id: int,
         hotel_name: str | None = Body(default=None, embed=True),
         city: str | None = Body(default=None, embed=True)
 ):
@@ -75,6 +65,16 @@ def partial_edit_hotel(
                     hotels_db[index]["city"] = city
                 return {"status": 200, "message": "OK"}
     return {"status": 422, "message": "Bad Data"}
+
+
+@app.delete("/hotels/{hotel_id}", summary="delete hotel")
+def delete_hotel(hotel_id: int):
+    """ delete hotel by key hotel_id """
+    for index, hotel in enumerate(hotels_db):
+        if hotel["id"] == hotel_id:
+            hotels_db.pop(index)
+            return {"status": 200, "message": "OK"}
+    return {"status": 404, "message": "Not Found"}
 
 
 if __name__ == "__main__":
