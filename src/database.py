517adcc9075a67cf8819bff8ec0fe083748e16b3
Magomedow Ruslan <main@mail.ru>
2025-04-13 15:54:57 +0300

    create new file src/models/hotels.py

--- src/database.py
+++ src/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
-import asyncio
+from sqlalchemy.orm import DeclarativeBase
 
 from src.config import settings
 
 
 engine = create_async_engine(settings.DB_URL)
+session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
